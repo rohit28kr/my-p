72d9533b09123d870597430ec7e8dd409eb6316a
rohit28kr <rohitkr20082004@gmail.com>
2024-12-03 13:02:49 +0530

    Add files via upload

---
+++ associate.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<pre>
+<?php  
+$cars = array("brand" => "Ford", "model" => "Mustang");
+$cars += ["color" => "red", "year" => 2022];
+
+var_dump($cars);
+?>
+
+</pre>
+
+</body>
